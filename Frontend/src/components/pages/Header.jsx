6879855d4e1ce3421cb7bb6cb5875ba338591b49
Devang Singh Parihar <151598328+dev-8085@users.noreply.github.com>
2024-10-25 08:35:59 -0700

    added about

--- Frontend/src/components/pages/Header.jsx
+++ Frontend/src/components/pages/Header.jsx
@@ -5,13 +5,14 @@ const Header = () => {
   return (
     <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px \-10 lg:px-20'>   
          {/*------Left Side-------*/}
-    <div>
-    <p className=' md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
+    
+    <div className=' md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
+      <p className= 'text-3x1 md:text-4x1 lg:text-5x1 text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
        Book Appointment <br/> With Trusted Doctors
     </p>
     <div>
-        <img src={assets.group-profiles} alt="" />
-        <p>Simply browse through our extensive list of trusted doctors,schedule your appointments hassle-free</p>
+        <img src={assets.group_profiles} alt="" />
+        <p>Simply browse through our extensive list of trusted doctors,<br/>schedule your appointments hassle-free</p>
     </div>
     <a href="">
         Book appointment <img src={assets.arrow_icon} alt="" />
